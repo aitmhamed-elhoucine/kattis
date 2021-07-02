76ae5a8d2c90d7dbd73613ee7c8996f3df892759
El houcine AIT M'HAMED <56789217+aitmhamed-elhoucine@users.noreply.github.com>
2021-07-02 09:36:58 +0100

    Create ADifferentProblem.c

---
+++ ADifferentProblem.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    long long a, b;
+    while (scanf("%lld%lld", &a, &b) == 2) {
+        (a>b) ? printf("%lld\n",a-b) : printf("%lld\n",b-a);
+    }
+    return 0;
+}
