d2f4f84f64e50586d38a24f67e0e06dbbbc56c35
El houcine AIT M'HAMED <56789217+aitmhamed-elhoucine@users.noreply.github.com>
2021-07-04 13:18:47 +0100

    Create 4thought.c

---
+++ 4thought.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+
+void equation4( int m){
+    if (4 + 4 + 4 + 4 == m)  printf("4 + 4 + 4 + 4 = %d \n",m);
+    else if (4 + 4 + 4 - 4 == m)  printf("4 + 4 + 4 - 4 = %d \n",m);
+    else if (4 + 4 + 4 * 4 == m)  printf("4 + 4 + 4 * 4 = %d \n",m);
+    else if (4 + 4 + 4 / 4 == m)  printf("4 + 4 + 4 / 4 = %d \n",m);
+    
+    else if (4 + 4 - 4 + 4 == m)  printf("4 + 4 - 4 + 4 = %d \n",m);
+    else if (4 + 4 - 4 - 4 == m)  printf("4 + 4 - 4 - 4 = %d \n",m);
+    else if (4 + 4 - 4 * 4 == m)  printf("4 + 4 - 4 * 4 = %d \n",m);
+    else if (4 + 4 - 4 / 4 == m)  printf("4 + 4 - 4 / 4 = %d \n",m);
+    
+    else if (4 + 4 * 4 + 4 == m)  printf("4 + 4 * 4 + 4 = %d \n",m);
+    else if (4 + 4 * 4 - 4 == m)  printf("4 + 4 * 4 - 4 = %d \n",m);
+    else if (4 + 4 * 4 * 4 == m)  printf("4 + 4 * 4 * 4 = %d \n",m);
+    else if (4 + 4 * 4 / 4 == m)  printf("4 + 4 * 4 / 4 = %d \n",m);
+    
+    else if (4 + 4 / 4 + 4 == m)  printf("4 + 4 / 4 + 4 = %d \n",m);
+    else if (4 + 4 / 4 - 4 == m)  printf("4 + 4 / 4 - 4 = %d \n",m);
+    else if (4 + 4 / 4 * 4 == m)  printf("4 + 4 / 4 * 4 = %d \n",m);
+    else if (4 + 4 / 4 / 4 == m)  printf("4 + 4 / 4 / 4 = %d \n",m);
+    
+    else if (4 - 4 + 4 + 4 == m)  printf("4 - 4 + 4 + 4 = %d \n",m);
+    else if (4 - 4 + 4 - 4 == m)  printf("4 - 4 + 4 - 4 = %d \n",m);
+    else if (4 - 4 + 4 * 4 == m)  printf("4 - 4 + 4 * 4 = %d \n",m);
+    else if (4 - 4 + 4 / 4 == m)  printf("4 - 4 + 4 / 4 = %d \n",m);
+    
+    else if (4 - 4 - 4 + 4 == m)  printf("4 - 4 - 4 + 4 = %d \n",m);
+    else if (4 - 4 - 4 - 4 == m)  printf("4 - 4 - 4 - 4 = %d \n",m);
+    else if (4 - 4 - 4 * 4 == m)  printf("4 - 4 - 4 * 4 = %d \n",m);
+    else if (4 - 4 - 4 / 4 == m)  printf("4 - 4 - 4 / 4 = %d \n",m);
+    
+    else if (4 - 4 * 4 + 4 == m)  printf("4 - 4 * 4 + 4 = %d \n",m);
+    else if (4 - 4 * 4 - 4 == m)  printf("4 - 4 * 4 - 4 = %d \n",m);
+    else if (4 - 4 * 4 * 4 == m)  printf("4 - 4 * 4 * 4 = %d \n",m);
+    else if (4 - 4 * 4 / 4 == m)  printf("4 - 4 * 4 / 4 = %d \n",m);
+    
+    else if (4 - 4 / 4 + 4 == m)  printf("4 + 4 / 4 + 4 = %d \n",m);
+    else if (4 - 4 / 4 - 4 == m)  printf("4 + 4 / 4 - 4 = %d \n",m);
+    else if (4 - 4 / 4 * 4 == m)  printf("4 + 4 / 4 * 4 = %d \n",m);
+    else if (4 - 4 / 4 / 4 == m)  printf("4 + 4 / 4 / 4 = %d \n",m);
+    
+    else if (4 * 4 + 4 + 4 == m)  printf("4 * 4 + 4 + 4 = %d \n",m);
+    else if (4 * 4 + 4 - 4 == m)  printf("4 * 4 + 4 - 4 = %d \n",m);
+    else if (4 * 4 + 4 * 4 == m)  printf("4 * 4 + 4 * 4 = %d \n",m);
+    else if (4 * 4 + 4 / 4 == m)  printf("4 * 4 + 4 / 4 = %d \n",m);
+    
+    else if (4 * 4 - 4 + 4 == m)  printf("4 * 4 - 4 + 4 = %d \n",m);
+    else if (4 * 4 - 4 - 4 == m)  printf("4 * 4 - 4 - 4 = %d \n",m);
+    else if (4 * 4 - 4 * 4 == m)  printf("4 * 4 - 4 * 4 = %d \n",m);
+    else if (4 * 4 - 4 / 4 == m)  printf("4 * 4 - 4 / 4 = %d \n",m);
+    
+    else if (4 * 4 * 4 + 4 == m)  printf("4 * 4 * 4 + 4 = %d \n",m);
+    else if (4 * 4 * 4 - 4 == m)  printf("4 * 4 * 4 - 4 = %d \n",m);
+    else if (4 * 4 * 4 * 4 == m)  printf("4 * 4 * 4 * 4 = %d \n",m);
+    else if (4 * 4 * 4 / 4 == m)  printf("4 * 4 * 4 / 4 = %d \n",m);
+    
+    else if (4 * 4 / 4 + 4 == m)  printf("4 * 4 / 4 + 4 = %d \n",m);
+    else if (4 * 4 / 4 - 4 == m)  printf("4 * 4 / 4 - 4 = %d \n",m);
+    else if (4 * 4 / 4 * 4 == m)  printf("4 * 4 / 4 * 4 = %d \n",m);
+    else if (4 * 4 / 4 / 4 == m)  printf("4 * 4 / 4 / 4 = %d \n",m);
+    
+    else if (4 / 4 + 4 + 4 == m)  printf("4 / 4 + 4 + 4 = %d \n",m);
+    else if (4 / 4 + 4 - 4 == m)  printf("4 / 4 + 4 - 4 = %d \n",m);
+    else if (4 / 4 + 4 * 4 == m)  printf("4 / 4 + 4 * 4 = %d \n",m);
+    else if (4 / 4 + 4 / 4 == m)  printf("4 / 4 + 4 / 4 = %d \n",m);
+    
+    else if (4 / 4 - 4 + 4 == m)  printf("4 / 4 - 4 + 4 = %d \n",m);
+    else if (4 / 4 - 4 - 4 == m)  printf("4 / 4 - 4 - 4 = %d \n",m);
+    else if (4 / 4 - 4 * 4 == m)  printf("4 / 4 - 4 * 4 = %d \n",m);
+    else if (4 / 4 - 4 / 4 == m)  printf("4 / 4 - 4 / 4 = %d \n",m);
+    
+    else if (4 / 4 * 4 + 4 == m)  printf("4 / 4 * 4 + 4 = %d \n",m);
+    else if (4 / 4 * 4 - 4 == m)  printf("4 / 4 * 4 - 4 = %d \n",m);
+    else if (4 / 4 * 4 * 4 == m)  printf("4 / 4 * 4 * 4 = %d \n",m);
+    else if (4 / 4 * 4 / 4 == m)  printf("4 / 4 * 4 / 4 = %d \n",m);
+    
+    else if (4 / 4 / 4 + 4 == m)  printf("4 / 4 / 4 + 4 = %d \n",m);
+    else if (4 / 4 / 4 - 4 == m)  printf("4 / 4 / 4 - 4 = %d \n",m);
+    else if (4 / 4 / 4 * 4 == m)  printf("4 / 4 / 4 * 4 = %d \n",m);
+    else if (4 / 4 / 4 / 4 == m)  printf("4 / 4 / 4 / 4 = %d \n",m);
+    
+    else printf("no solution \n");
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0; i<n ; i++){
+        int m;
+        scanf("%d",&m);
+        if ((m > 256) || (m < -60)){
+            printf("no solution\n");
+        }
+        else{
+            equation4(m);
+        }
+        
+    }
+    return 0;
+}
